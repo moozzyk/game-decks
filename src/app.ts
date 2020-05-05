114a9b28e89b303c562aead5fdf10d6915dc41c1
moozzyk <moozzyk@gmail.com>
2020-05-04 22:38:19 -0700

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => console.log("Server started."));
+
