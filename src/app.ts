9194e01e8fd8076888090b3000a2c6147d71f20c
moozzyk <moozzyk@gmail.com>
2020-05-06 21:40:01 -0700

    Add GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import express, { Application, Request, Response } from "express";
 import process from "process";
+import { buildSchema } from "graphql";
+import graphqlHTTP from "express-graphql";
+
+const schema = buildSchema(`
+type Query {
+  hello: String
+}`);
+
+const root = {
+  hello: () => {
+    return "Hello, World!";
+  },
+};
 
 const app: Application = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello");
