b9ab1f0e93b482981d1923d93bf005108fa2aeca
moozzyk <moozzyk@gmail.com>
2020-05-12 22:38:28 -0700

    Enable discarding cards

--- src/core/deck.ts
+++ src/core/deck.ts
@@ -1,16 +1,24 @@
-export type Card = {
-  name: string;
-  description: string;
-};
+export class Card {
+  constructor(readonly name: string, readonly description: string, readonly deck: Deck) {}
+
+  discard() {
+    this.deck.discard(this);
+  }
+}
 
 export class Deck {
-  constructor(readonly name: string, readonly cards: Card[]) {}
+  readonly cards: Card[];
+  readonly discardPile: Card[] = [];
+
+  constructor(readonly name: string, readonly cardsDefinitions: { name: string; description: string }[]) {
+    this.cards = cardsDefinitions.map((c) => new Card(c.name, c.description, this));
+  }
 
   get isEmpty(): Boolean {
     return this.cards.length === 0;
   }
 
-  shuffle(): void {
+  shuffle() {
     for (let i = this.cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
@@ -19,5 +27,16 @@ export class Deck {
 
   draw(): Card | undefined {
     return this.cards.shift();
+  }
+
+  discard(card: Card) {
+    if (card.deck !== this) {
+      throw new Error("Cannot discard a from a different deck");
+    }
+
+    if (this.cards.includes(card)) {
+      throw new Error("Cannot discard a card that is still in the deck");
+    }
+    this.discardPile.push(card);
   }
 }
--- test/core/deck.spec.ts
+++ test/core/deck.spec.ts
@@ -1,14 +1,34 @@
 import { Deck } from "../../src/core/deck";
 
-describe("isEmpty", () => {
-  it("should be false if deck not empty", () => {
-    const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
-    expect(deck.isEmpty).toBe(false);
+describe("Deck", () => {
+  describe("isEmpty", () => {
+    it("should be false if deck not empty", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
+      expect(deck.isEmpty).toBe(false);
+    });
+
+    it("should be true after drawing last card", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
+      deck.draw();
+      expect(deck.isEmpty).toBe(true);
+    });
   });
 
-  it("should be true after drawing last card", () => {
-    const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
-    deck.draw();
-    expect(deck.isEmpty).toBe(true);
+  describe("discard", () => {
+    it("discard pile should be empty by default", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
+      expect(deck.discardPile).toStrictEqual([]);
+    });
+  });
+});
+
+describe("Card", () => {
+  describe("discard", () => {
+    it("discard pile should contain discarded card", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
+      const card = deck.draw()!;
+      card.discard();
+      expect(deck.discardPile).toStrictEqual([card]);
+    });
   });
 });
