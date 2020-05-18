e2667b4f2c0fddad37f145078a9172a28220271a
moozzyk <moozzyk@gmail.com>
2020-05-17 22:04:28 -0700

    Refactor all things

--- src/core/deck.ts
+++ src/core/deck.ts
@@ -6,37 +6,71 @@ export class Card {
   }
 }
 
-export class Deck {
-  readonly drawPile: Card[];
-  readonly discardPile: Card[] = [];
+export class CardCollection {
+  constructor(private readonly cards: Card[]) {}
 
-  constructor(readonly name: string, readonly cardsDefinitions: { name: string; description: string }[]) {
-    this.drawPile = cardsDefinitions.map((c) => new Card(c.name, c.description, this));
+  draw(): Card | undefined {
+    return this.cards.shift();
+  }
+
+  hasCard(card: Card): Boolean {
+    return this.cards.includes(card);
+  }
+
+  add(card: Card) {
+    this.cards.push(card);
+  }
+
+  remove(card: Card): Boolean {
+    const index = this.cards.findIndex((c) => c === card);
+    if (index >= 0) {
+      this.cards.splice(index, 1);
+      return true;
+    }
+    return false;
   }
 
   get isEmpty(): Boolean {
-    return this.drawPile.length === 0;
+    return this.cards.length === 0;
   }
 
-  shuffle() {
-    for (let i = this.drawPile.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.drawPile[i], this.drawPile[j]] = [this.drawPile[j], this.drawPile[i]];
-    }
+  get showCards(): Card[] {
+    return this.cards;
+  }
+}
+
+export class Deck {
+  readonly drawPile: CardCollection;
+  readonly discardPile: CardCollection;
+
+  constructor(readonly name: string, cardDefinitions: { name: string; description?: string }[]) {
+    this.drawPile = new CardCollection(cardDefinitions.map((c) => new Card(c.name, c.description || "", this)));
+    this.discardPile = new CardCollection([]);
   }
 
   draw(): Card | undefined {
-    return this.drawPile.shift();
+    return this.drawPile.draw();
   }
 
   discard(card: Card) {
     if (card.deck !== this) {
-      throw new Error("Cannot discard a from a different deck");
+      throw new Error("Cannot discard a card from a different deck");
     }
 
-    if (this.drawPile.includes(card)) {
-      throw new Error("Cannot discard a card that is still in the deck");
+    if (this.drawPile.hasCard(card)) {
+      throw new Error("Cannot discard a card that is still in the draw pile");
     }
-    this.discardPile.push(card);
+    this.discardPile.add(card);
+  }
+
+  shuffle() {
+    shuffle(this.drawPile.showCards);
+  }
+}
+
+function shuffle(cards: Card[]) {
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
   }
 }
--- test/core/deck.spec.ts
+++ test/core/deck.spec.ts
@@ -1,23 +1,49 @@
 import { Deck } from "../../src/core/deck";
 
 describe("Deck", () => {
-  describe("isEmpty", () => {
-    it("should be false if deck not empty", () => {
-      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
-      expect(deck.isEmpty).toBe(false);
+  describe("drawPile", () => {
+    it("should not be false if contains cards", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace" }]);
+      expect(deck.drawPile.isEmpty).toBe(false);
     });
 
-    it("should be true after drawing last card", () => {
-      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
+    it("should be empty after drawing last card", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace" }]);
       deck.draw();
-      expect(deck.isEmpty).toBe(true);
+      expect(deck.discardPile.isEmpty).toBe(true);
     });
   });
 
-  describe("discard", () => {
+  describe("discardPile", () => {
     it("discard pile should be empty by default", () => {
-      const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
-      expect(deck.discardPile).toStrictEqual([]);
+      const deck = new Deck("test-deck", [{ name: "Ace" }]);
+      expect(deck.discardPile.isEmpty).toBe(true);
+    });
+
+    it("discard pile should not be empty after card is discarded", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace" }]);
+      const card = deck.draw();
+      deck.discard(card!);
+      expect(deck.discardPile.isEmpty).toBe(false);
+    });
+  });
+
+  describe("draw", () => {
+    it("returns undefined if draw pile empty", () => {
+      const deck = new Deck("test-deck", [{ name: "Ace" }]);
+      expect(deck.draw()).not.toBeUndefined();
+      expect(deck.draw()).toBeUndefined();
+    });
+  });
+
+  describe("shuffle", () => {
+    it("rearranges cards", () => {
+      const randomMock = jest.spyOn(global.Math, "random").mockReturnValue(0.2);
+      const deck = new Deck("test-deck", [{ name: "Ace" }, { name: "Joker" }]);
+      const originalOrder = [...deck.drawPile.showCards];
+      deck.shuffle();
+      expect(deck.drawPile.showCards).not.toStrictEqual(originalOrder);
+      randomMock.mockRestore();
     });
   });
 });
@@ -28,7 +54,7 @@ describe("Card", () => {
       const deck = new Deck("test-deck", [{ name: "Ace", description: "" }]);
       const card = deck.draw()!;
       card.discard();
-      expect(deck.discardPile).toStrictEqual([card]);
+      expect(deck.discardPile.showCards).toStrictEqual([card]);
     });
   });
 });
